1c666678049c30e724f240a7392c91f76a1d778d
室井博也 <>
2020-09-02 15:30:50 +0900

    rubocop 文法チェックツールの導入

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update]
-  
+
   def index
     @articles = Article.all
   end
@@ -28,7 +28,7 @@ class ArticlesController < ApplicationController
   def update
     if @article.update(article_params)
       redirect_to article_path(@article), notice: '更新できました'
-    else 
+    else
       flash.now[:erorr] = '更新できませんでした'
       render :edit
     end
