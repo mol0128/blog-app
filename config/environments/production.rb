0fdfce96effaa7024f8eaafdbe3f38bcf916d036
室井博也 <>
2020-09-28 12:39:11 +0900

    hostを.ENV

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,8 +62,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  host = 'https://blog-app-molmol.herokuapp.com/'
-  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.default_url_options = { host: ENV['HOST'] }
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
